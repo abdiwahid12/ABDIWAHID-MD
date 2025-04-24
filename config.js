a31212bbe6399859d1b365c999ea23a282d0c963
abdiwahid12 <wahidabdi891@gmail.com>
2025-04-24 08:50:54 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  owner: process.env.OWNER_NUMBER,
-  botName: process.env.BOT_NAME || 'ABDIWAHID-MD',
-  weatherApiKey: process.env.WEATHER_API_KEY,
-  newsApiKey: process.env.NEWS_API_KEY,
-  prefix: process.env.PREFIX || '!',
-};
